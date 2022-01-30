0a593a850e28e80002604956deb35926508710bb
roberto montalti <roberto.montalti00@gmail.com>
2022-01-30 17:26:34 +0100

    Removed some unneeded files, added an iframe to diplay 3d skins

---
+++ db_utils/index.js
@@ -0,0 +1,38 @@
+import { SteamMarketParser } from 'steam-market-parser';
+import fetch from "node-fetch";
+import fs from "fs";
+
+const getSkins = async (offset, limit) => {
+    let buildUrl = (offset) =>
+        `https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=40&isStore=true&limit=${limit}&maxPrice=10000&minPrice=1&offset=${offset}&withStack=true`;
+    return await fetch(buildUrl(offset))
+        .then(async res => await res.json());
+};
+
+const main = async () => {
+    let step = 60;
+    let foundItems = [];
+
+    let i = 0;
+    while (true) {
+        i += step;
+        try {
+            console.log("Searching offset =", i);
+            foundItems = [...foundItems, ...(await getSkins(i, step)).items];
+        } catch (_) {
+            fs.writeFileSync("test.json", JSON.stringify(foundItems));
+            break;
+        }
+    }
+
+};
+
+main();
+
+/*
+const smParser = new SteamMarketParser({appId: 730, currency: Currency.USD});
+
+(async () => {
+    const data = await smParser.getMarketData('Five-SeveN | Hyper Beast (Field-Tested)');
+})();
+*/
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,1 @@
-
-<h1>Ghisa shop</h1>
+<iframe src="https://3d.cs.money/item/Yx6j1lz" width="700px" height="400px" webkitallowfullscreen allow='camera; gyroscope; accelerometer; magnetometer; fullscreen;' ></iframe>
