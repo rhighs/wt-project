e270f97bfa388c15ad7668c1eae4827fd51defe6
roberto montalti <roberto.montalti00@gmail.com>
2022-02-08 14:31:14 +0100

    Error page

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="{{ url('assets/css/root.css') }}"/>
 <link rel="stylesheet" href="{{ url('assets/css/error.css') }}"/>
+<link rel="stylesheet" type="text/css" href="https://unpkg.com/bulma-prefers-dark" />
 <script src="{{ url('assets/js/auth/auth.js') }}" type="text/javascript"></script>
 
 @include("navbar")
