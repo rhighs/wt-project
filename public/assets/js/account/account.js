2663ed811ff64f06dc19541c38030bbec21158b6
jackmonta <gmonta2000@gmail.com>
2022-02-11 16:11:52 +0100

    refactoring account page

--- public/assets/js/account/account.js
+++ public/assets/js/account/account.js
@@ -1,12 +1,114 @@
+let userId;
+
+const loadData = (user) => {
+    let userName = document.getElementById('user-name');
+    let userSurname = document.getElementById('user-surname');
+    let userId = document.getElementById('user-id');
+    let userEmail = document.getElementById('user-email');
+
+    userName.innerHTML = user.name;
+    userSurname.innerHTML = user.surname;
+    userId.innerHTML = user.id;
+    userEmail.innerHTML = user.email;
+
+    console.log(user);
+}
+
+const formCheck = () => {
+
+    let name = document.getElementById('form-name').value;
+    let surname = document.getElementById('form-surname').value;
+    let email = document.getElementById('form-email').value;
+    let password = document.getElementById('form-psw').value;
+    let confirmPassword = document.getElementById('form-confirm-psw').value;
+
+    let errors = []
+    let voids = []
+
+    if (!name) {
+        voids.push('nome');
+    }
+    if (!surname) {
+        voids.push('cognome');
+    }
+    if (!email) {
+        voids.push('email');
+    }
+    if (!password) {
+        voids.push('password');
+    }
+    if (!confirmPassword) {
+        voids.push('password di conferma');
+    }
+
+    if (!validateEmail(email)) {
+        errors.push('email non valida');
+    }
+    if (password != confirmPassword) {
+        errors.push('le password devono essere indetiche');
+    }
+
+    if (voids.length > 0) {
+        errors.push("I campi " + voids.join(', ') + " sono obbligatori");
+    }
+
+    if (errors.length > 0) {
+        displayErrors(errors, 'error-list');
+        return false;
+    }
+
+    return true;
+}
+
+const sendData = (data) => {
+    fetch('api/account/' + userId, { 
+        method: 'PATCH',
+        body: JSON.stringify({
+            name: data.name,
+            surname: data.surname,
+            email: data.email,
+            password: data.password
+        })
+    });
+}
+
+let modifyDataButton = document.getElementById('modifyCredentials');
+modifyDataButton.addEventListener("click", () => {
+    document.getElementById('modifyForm').style.display = "block";
+});
+
+let closeFormButton = document.getElementById('closeButton');
+closeFormButton.addEventListener('click', () => {
+    document.getElementById("modifyForm").style.display = "none";
+});
+
+let saveFormButton = document.getElementById('save-button');
+    saveFormButton.addEventListener('click', () => {
+    let check = formCheck();
+    if (check == false) { return; }
+
+    let name = document.getElementById('form-name').value;
+    let surname = document.getElementById('form-surname').value;
+    let email = document.getElementById('form-email').value;
+    let password = document.getElementById('form-psw').value;
+    
+    let data = {
+        name: name,
+        surname: surname,
+        email: email,
+        password: password
+    }
+    sendData(data);
+});
+
 redirectIfNotAuthenticated();
-
 testAuth()
-    .then(result => {
-        document.getElementById("name").innerHTML = result.name;
-        document.getElementById("surname").innerHTML = result.surname;
-        //document.getElementById("email").value = result.email;
+    .then(user => {
 
-        fetch("/api/transaction/" + result.id, {
+        userId = user.id;
+        loadData(user);
+
+        fetch("/api/transaction/" + user.id, {
             method: "POST"
         }).then(async result => {
             let jsonData = await result.json();
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,91 +1,121 @@
 <div class="container">
     <div class="columns">
-        <div class="column is-9">
+        <div class="column">
             <section class="hero is-info welcome is-medium">
                 <div class="hero-body">
                     <div class="container">
-                        <h1 class="title">
-                            Benvenuto,
-                            <span id="name">
-                                <!-- Nome qui -->
-                            </span>
-                            <span id="surname">
-                                <!-- Cognome qui -->
-                            </span>.
-                        </h1>
-                    </div>
-                </div>
-            </section>
-
-            <div class="columns">
-                <div class="column is-6">
-
-            <table class="table is-fullwidth">
-                <thead>
-                    <tr>
-                        <th><abbr title="Position">Id</abbr></th>
-                        <th><abbr title="Played">Id Transazione</abbr></th>
-                        <th><abbr title="Won">Prezzo</abbr></th>
-                        <th><abbr title="Drawn">Data</abbr></th>
-                        <th><abbr title="Lost">Numero carta</abbr></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td><a href="https://en.wikipedia.org/wiki/Leicester_City_F.C." title="Leicester City F.C.">Leicester City</a> <strong>(C)</strong>
-                        </td>
-                        <td>38</td>
-                        <td>23</td>
-                        <td>12</td>
-                    </tr>
-                </tbody>
-            </table>
-                </div>
-                <div class="column is-6">
-                    <div class="card">
-                        <div class="control"></div>
-                        <div class="card">
-                            <header class="card-header">
-                                <p class="card-header-title">
-                                    Order by
-                                </p>
-                                <a href="#" class="card-header-icon" aria-label="more options">
-                                    <span class="icon">
-                                        <i class="fa fa-angle-down" aria-hidden="true"></i>
+                        <div class="columns">
+                            <div class="column">
+                                <h1 class="title">
+                                Benvenuto,
+                                    <span id="name">
+                                        <!-- Nome qui -->
                                     </span>
-                                </a>
-                            </header>
-                            <div class="card-content">
-                                <div class="content">
-                                    <label class="radio">
-                                        <input type="radio" name="answer" />
-                                        Price: Low-High
-                                    </label><br></br>
-                                    <label class="radio">
-                                        <input type="radio" name="answer" />
-                                        Price: High-Low
-                                    </label><br></br>
-                                    <label class="radio">
-                                        <input type="radio" name="answer" />
-                                        Date: Old-Last
-                                    </label><br></br>
-                                    <label class="radio">
-                                        <input type="radio" name="answer" />
-                                        Date: Last-Old
-                                    </label><br></br>
-                                    <div id="account-button-search-container">
-                                        <button id="account-button" onclick="">Search</button>
+                                </h1>
+                            </div>
+                            <div class="column">
+                                <div class="container box">
+                                    <div class="row" id="userInfo"><b>
+                                        <span id="user-name">
+                                            <!-- Nome qui -->
+                                        </span>
+                                        <span id="user-surname">
+                                            <!-- surname qui -->
+                                        </span>
+                                    </b></div>
+                                    <div class="row">
+                                        ID Cliente:
+                                        <b><span id="user-id">
+                                            <!-- id qui -->
+                                        </span></b>
                                     </div>
-
+                                    <div class="row">
+                                        E-mail:
+                                        <b><span id="user-email">
+                                            <!-- email qui -->
+                                        </span></b>
+                                    </div>
+                                    <div class="row">
+                                        Password:
+                                        <b><span id="user-password">
+                                            ********
+                                        </span></b>
+                                    </div> 
+                                    <div class="row">
+                                        <button id="modifyCredentials" class="button is-dark">Modifica</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </section>
+            <div class="columns">
+                <div class="column">
+                    <table class="table is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th title="Position">Id</th>
+                                <th title="Played">Num. transazione</th>
+                                <th title="Won">Prezzo</th>
+                                <th title="Drawn">Data</th>
+                                <th title="Lost">Numero carta</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td><a href="https://en.wikipedia.org/wiki/Leicester_City_F.C." title="Leicester City F.C.">Leicester City</a> <strong>(C)</strong>
+                                </td>
+                                <td>€99</td>
+                                <td>XX/XX/20XX</td>
+                                <td>**** **** **** 1234</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modify-popup" id="modifyForm">
+    <div class="container box form">
+        <div class="row form-head">
+            <div class="title">Modifica dati account</div>
+            <div class="close-button-container is-pulled-right"><button id="closeButton" class="button is-dark">Close</button></div>
+        </div>
+        <div class="column form-data">
+            <b class="title">Dati personali:</b>
+            <div class="row">
+                <label for="name"><b>Nome *</b></label><br>
+                <input id="form-name" class="input" type="text" placeholder="Enter name" name="name" required><br>
+            </div>
+            <div class="row">
+                <label for="surname"><b>Cognome *</b></label><br>
+                <input id="form-surname" class="input" type="text" placeholder="Enter surname" name="surname" required><br>
+            </div>
+            <br><b class="title">Dati di accesso:</b>
+            <div class="row">
+                <label for="email"><b>Email *</b></label><br>
+                <input id="form-email" class="input" type="text" placeholder="Enter Email" name="email" required><br>
+            </div>
+            <div class="row">
+                <label for="psw"><b>Password *</b></label><br>
+                <input id="form-psw" class="input" type="password" placeholder="Enter Password" name="psw" required><br>
+            </div>
+            <div class="row">
+                <label for="psw"><b>Confirm Password *</b></label><br>
+                <input id="form-confirm-psw" class="input" type="password" placeholder="Confirm Password" name="confirm-psw" required><br>
+            </div>
+            <div class="row error-row">
+                <p id="error-list"></p>
+            </div>
+            <div class="row save-row">
+                <button id="save-button" class="button is-dark">Save</button><br>
             </div>
         </div>
     </div>
 </div>
 
 <script src="{{ url('assets/js/account/account.js') }}"></script>
+<script src="{{ url('assets/js/utils.js') }}"></script>
