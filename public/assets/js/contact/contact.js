daed0459322a4488eca3a46f997b60f95a803892
jurisimo00 <juri.simoncini@studio.unibo.it>
2022-02-11 16:09:14 +0100

    update contact

--- public/assets/js/contact/contact.js
+++ public/assets/js/contact/contact.js
@@ -1,10 +1,41 @@
 function send() {
-    document.getElementsByClassName("input is-medium")[0].value = "ciao";
-    document.getElementsByClassName("input is-medium")[1].value = "ciao";
-    document.getElementsByClassName("textarea is-medium")[0].value = "ciao";
-    /*const input = [];
-    input.push = document.getElementsByClassName("input is-medium") ;
-    for (let i = 0; i < input.length; i++) {
-        input[i].value = "ciao";
-    } */   
+    let username = document.getElementsByClassName("input is-medium")[0];
+    let email = document.getElementsByClassName("input is-medium")[1];
+    let message = document.getElementsByClassName("textarea is-medium")[0];
+    if(checkInput(username,email,message)){
+        simpleNotify.notify("Messaggio inviato!");
+        clearAll(username,email,message);
+    }else{
+        simpleNotify.notify("Messaggio non inviato!","danger");   
+    }
+}
+
+function checkString(str) {
+    return (!str && str.length === 0)
+}
+
+function checkInput(username,email,message) {
+    
+    if(checkString(username.value)){
+        username.value="";
+        username.setAttribute("placeholder","Nome mancante!");
+        return false;
+    }
+    if(!validateEmail(email.value)){
+        email.value="";
+        email.setAttribute("placeholder","Email errata!");
+        return false;
+    }
+    if(checkString(message.value)){
+        message.value="";
+        message.setAttribute("placeholder","Messaggio mancante!");
+        return false;
+    }
+    return true;
+}
+
+function clearAll(username, email, message) {
+    username.value = "";
+    email.value = "";
+    message.value = "";
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,22 +1,24 @@
 <script src="{{ url('assets/js/contact/map.js') }}"></script>
 <script src="{{ url('assets/js/contact/contact.js') }}"></script>
-
+<script src="{{ url('assets/js/utils.js')}}" type="text/javascript"></script>
 
 <section class="hero is-medium">
 	<div class="hero-body">
 		<div class="container has-text-centered">
 			<div class="columns is-8 is-variable ">
 				<div class="column is-two-thirds has-text-left">
-					<h1 class="title is-1">Contact Us</h1>
-					<p class="is-size-4">Vienici a trovare in sede! (scriverci altre due cazzate porco dio)</p>
-						<div id="map"></div>
+					<h1 class="title is-1">Contattaci!</h1>
+					<p class="is-size-4">Vienici a trovare in sede!</p>
+					<p class="is-size-4">Altrimenti compila il form qui di fianco e saremo felici di risponderti!</p>
+					<br></br>
+					<div id="map"></div>
 
 						<!-- Async script executes immediately and must be after any DOM elements used in callback. -->
 						<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKiJI2hPrm3xzrL7sZHyt_38-CzSjwoGU&callback=initMap" async></script>
 				</div>
 				<div class="column is-one-third has-text-left">
 					<div class="field">
-						<label class="label">Name</label>
+						<label class="label">Nome</label>
 						<div class="control">
 							<input class="input is-medium" type="text">
 						</div>
@@ -28,13 +30,13 @@
 						</div>
 					</div>
 					<div class="field">
-						<label class="label">Message</label>
+						<label class="label">Messaggio</label>
 						<div class="control">
 							<textarea class="textarea is-medium"></textarea>
 						</div>
 					</div>
 					<div class="control">
-						<button type="submit" onclick="send()" class="button is-link is-fullwidth has-text-weight-medium is-medium">Send Message</button>
+						<button type="submit" onclick="send()" class="button is-link is-fullwidth has-text-weight-medium is-medium">Invia</button>
 					</div>
 				</div>
 			</div>
